ff1b09cdb29e975eb9835dbd7474b45dbe8d4038
SofiaVytrishko <sofiia.vytrishko@nure.ua>
2016-12-12 18:28:22 +0200

    fifth lab commit

---
+++ usermanagement/src/main/java/com/vitrishko/usermanagement/db/UserDao.java
@@ -0,0 +1,16 @@
+package com.vitrishko.usermanagement.db;
+
+import java.util.Collection;
+
+import com.vitrishko.usermanagement.User;
+
+public interface UserDao {
+	User create(User user) throws DatabaseException;
+	void update (User user) throws DatabaseException;
+	void delete (User user) throws DatabaseException;
+	User find(Long id) throws DatabaseException;
+	Collection findAll() throws DatabaseException;
+	Collection find(String firstName, String lastName) throws DatabaseException;
+	void setConnectionFactory(ConnectionFactory connectionFactory);
+
+}
